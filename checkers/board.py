9a638b8b4a796a578c82289260f45dabb17f759d
AD001\z0049jfs <hidde.de_vries@siemens.com>
2022-03-30 16:44:38 +0200

    add movement of the piece

--- checkers/board.py
+++ checkers/board.py
@@ -17,6 +17,24 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def move(self, piece, row, col):
+        # easy way to swap things in a list, put it in reverse
+        # self.board[piece.row][piece.col] moves to self.board[row][col] and vice versa
+        # this means the piece moves to a place and the empty slot also moves
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col] 
+        piece.move(row, col)
+
+        #make sure if the piece becomes a king when hitting the first or last row
+        if row == ROWS or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
+
+    def get_piece(self, row, col):
+        return self.board[row][col]
+
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
