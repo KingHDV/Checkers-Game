3d167c31f26f16fd603b7ff12eff6127c3095701
KingHDV <64663463+KingHDV@users.noreply.github.com>
2022-04-01 16:57:17 +0200

    add mouse selection of a piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT
 from checkers.board import Board
 
 FPS = 60 #max frames per second
@@ -7,13 +7,16 @@ FPS = 60 #max frames per second
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = int(y / SQUARE_SIZE)
+    col = int(x / SQUARE_SIZE)
+    return row, col
+
 def main():
     run = True
     clock = pygame.time.Clock()
     board = Board()
-
-    piece = board.get_piece(0,1)
-    board.move(piece, 4, 3)
 
     while run:
         clock.tick(FPS)
@@ -22,11 +25,14 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
         
         board.draw(WIN)
         pygame.display.update()
         
     pygame.quit()
-
+    
 main()
